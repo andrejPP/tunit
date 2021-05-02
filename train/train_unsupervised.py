7e25cb5bc4441ab5d24241b11de2ea841d1b4584
andrejPP <andy.p.293@gmail.com>
2021-05-03 00:43:23 +0200

    small fix

--- train/train_unsupervised.py
+++ train/train_unsupervised.py
@@ -124,9 +124,11 @@ def trainGAN_UNSUP(data_loader, networks, opts, epoch, args, additional):
                 x_fake = G.decode(c_src, s_ref)
 
             x_ref.requires_grad_()
+            x_fake = x_fake.cuda(args.gpu)
+            x_fake.requires_grad_()
 
             d_real_logit, _ = D(x_ref, y_ref)
-            d_fake_logit, _ = D(x_fake.detach(), y_ref)
+            d_fake_logit, _ = D(x_fake, y_ref)
 
             d_adv_real = calc_adv_loss(d_real_logit, 'd_real')
             d_adv_fake = calc_adv_loss(d_fake_logit, 'd_fake')
