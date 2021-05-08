f3b35981f8c102e0b0d4dd68ed6219fe3d217ea8
xjanik20 <luo96@seznam.cz>
2021-05-08 23:28:18 +0200

    Residual guiding network

--- models/guidingNet.py
+++ models/guidingNet.py
@@ -3,6 +3,8 @@ TUNIT: Truly Unsupervised Image-to-Image Translation
 Copyright (c) 2020-present NAVER Corp.
 MIT license 
 """
+import math
+
 from torch import nn
 import torch.nn.functional as F
 
@@ -12,29 +14,84 @@ except:
     from blocks import Conv2dBlock, FRN
 
 
-cfg = {
-    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-    'vgg19cut': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'N'],
-}
+# cfg = {
+#     'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+#     'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+#     'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+#     'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+#     'vgg19cut': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'N'],
+# }
 
+class ResBlk(nn.Module):
+    def __init__(self, dim_in, dim_out, actv=nn.LeakyReLU(0.2),
+                 normalize=False, downsample=False):
+        super().__init__()
+        self.actv = actv
+        self.normalize = normalize
+        self.downsample = downsample
+        self.learned_sc = dim_in != dim_out
+        self._build_weights(dim_in, dim_out)
+
+    def _build_weights(self, dim_in, dim_out):
+        self.conv1 = nn.Conv2d(dim_in, dim_in, 3, 1, 1)
+        self.conv2 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
+        if self.normalize:
+            self.norm1 = nn.InstanceNorm2d(dim_in, affine=True)
+            self.norm2 = nn.InstanceNorm2d(dim_in, affine=True)
+        if self.learned_sc:
+            self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
+
+    def _shortcut(self, x):
+        if self.learned_sc:
+            x = self.conv1x1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        return x
+
+    def _residual(self, x):
+        if self.normalize:
+            x = self.norm1(x)
+        x = self.actv(x)
+        x = self.conv1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        if self.normalize:
+            x = self.norm2(x)
+        x = self.actv(x)
+        x = self.conv2(x)
+        return x
+
+    def forward(self, x):
+        x = self._shortcut(x) + self._residual(x)
+        return x / math.sqrt(2)  # unit variance
 
 class GuidingNet(nn.Module):
-    def __init__(self, img_size=64, output_k={'cont': 128, 'disc': 10}):
+    def __init__(self, img_size=128, output_k={'cont': 128, 'disc': 10}, max_conv_dim=512):
         super(GuidingNet, self).__init__()
         # network layers setting
-        self.features = make_layers(cfg['vgg11'], True)
+        dim_in = 2 ** 14 // img_size
+        blocks = []
+        blocks += [nn.Conv2d(3, dim_in, 3, 1, 1)]
 
-        self.disc = nn.Linear(512, output_k['disc'])
-        self.cont = nn.Linear(512, output_k['cont'])
+        repeat_num = int(np.log2(img_size)) - 2
+        for _ in range(repeat_num):
+            dim_out = min(dim_in * 2, max_conv_dim)
+            blocks += [ResBlk(dim_in, dim_out, downsample=True)]
+            dim_in = dim_out
 
-        self._initialize_weights()
+        blocks += [nn.LeakyReLU(0.2)]
+        blocks += [nn.Conv2d(dim_out, dim_out, 4, 1, 0)]
+        blocks += [nn.LeakyReLU(0.2)]
+
+        self.features = nn.Sequential(*blocks)
+
+        self.disc = nn.Linear(max_conv_dim, output_k['disc'])
+        self.cont = nn.Linear(max_conv_dim, output_k['cont'])
+
+        # self._initialize_weights()
 
     def forward(self, x, sty=False):
         x = self.features(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
         flat = x.view(x.size(0), -1)
         cont = self.cont(flat)
         if sty:
@@ -42,48 +99,46 @@ class GuidingNet(nn.Module):
         disc = self.disc(flat)
         return {'cont': cont, 'disc': disc}
 
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+    # def _initialize_weights(self):
+    #     for m in self.modules():
+    #         if isinstance(m, nn.Conv2d):
+    #             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+    #             if m.bias is not None:
+    #                 nn.init.constant_(m.bias, 0)
+    #         elif isinstance(m, nn.BatchNorm2d):
+    #             nn.init.constant_(m.weight, 1)
+    #             nn.init.constant_(m.bias, 0)
+    #         elif isinstance(m, nn.Linear):
+    #             nn.init.normal_(m.weight, 0, 0.01)
+    #             nn.init.constant_(m.bias, 0)
 
     def moco(self, x):
         x = self.features(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
         flat = x.view(x.size(0), -1)
         cont = self.cont(flat)
         return cont
 
     def iic(self, x):
         x = self.features(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
         flat = x.view(x.size(0), -1)
         disc = self.disc(flat)
         return disc
 
 
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=False)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=False)]
-            in_channels = v
-    return nn.Sequential(*layers)
+# def make_layers(cfg, batch_norm=False):
+#     layers = []
+#     in_channels = 3
+#     for v in cfg:
+#         if v == 'M':
+#             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+#         else:
+#             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+#             if batch_norm:
+#                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=False)]
+#             else:
+#                 layers += [conv2d, nn.ReLU(inplace=False)]
+#             in_channels = v
+#     return nn.Sequential(*layers)
 
 
 if __name__ == '__main__':
